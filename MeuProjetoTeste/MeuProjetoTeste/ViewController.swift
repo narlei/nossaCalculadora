380bac20d9a53cde8f38f2247547404fb2072973
Narlei Moreira <narlei.guitar@gmail.com>
2020-09-02 11:13:14 -0300

    Add project starter

---
+++ MeuProjetoTeste/MeuProjetoTeste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeuProjetoTeste
+//
+//  Created by Narlei A Moreira on 02/09/20.
+//  Copyright © 2020 Narlei A Moreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
